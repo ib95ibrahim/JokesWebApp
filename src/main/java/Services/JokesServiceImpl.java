34f4818b3306bf05b6eb77c0bb89da103661d49e
ibrahim zeroual <ib.zeroual@outlook.com>
2022-09-09 10:40:57 +0100

    Creating JokeInterface and its implementation.

---
+++ src/main/java/Services/JokesServiceImpl.java
@@ -0,0 +1,18 @@
+package Services;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JokesServiceImpl implements JokesService {
+    private final ChuckNorrisQuotes chuckNorrisQuotes;
+
+    public JokesServiceImpl(ChuckNorrisQuotes chuckNorrisQuotes) {
+        this.chuckNorrisQuotes = chuckNorrisQuotes;
+    }
+
+    @Override
+    public String getJoke() {
+        return chuckNorrisQuotes.getRandomQuote();
+    }
+}
